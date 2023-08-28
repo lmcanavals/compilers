0f7cf977a9b8615a313527b531f7fe4e14bd9374
Luis M Canaval S <public02@canaval.org>
2023-08-28 11:16:08 -0500

    bison calc

---
+++ week03_parsers/calclexer.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM 256
+#define YYEOF 399
+
+double yylval;
+
+int yylex(void) {
+  int c = getchar();
+  while (c == ' ' || c == '\t')
+    c = getchar();
+
+  if (c == '.' || isdigit(c)) {
+    ungetc(c, stdin);
+    if (scanf("%lf", &yylval) != 1)
+      abort();
+    return NUM;
+  } else if (c == EOF)
+    return YYEOF;
+  else
+    return c;
+}
