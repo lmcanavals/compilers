66bb942c5a0a9bc42d61a9a12a4a09ceb75af535
Luis M Canaval S <public02@canaval.org>
2023-09-29 11:20:25 -0500

    copilers compilers

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-source $HOME/Apps/vcopilers/bin/activate
+source $HOME/Apps/vcompilers/bin/activate
 export CLASSPATH=.:~/.m2/repository/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar:$CLASSPATH
 alias grun='java org.antlr.v4.gui.TestRig'
