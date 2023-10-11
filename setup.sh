0f5e4782e4d12c1169a0f8e4afae863f925c1022
Luis M Canaval S <public02@canaval.org>
2023-10-11 08:16:31 -0500

    antlr4 from scratch like a boss

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/zsh
 
-source $HOME/Apps/vcompilers/bin/activate
-export CLASSPATH=.:~/.m2/repository/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar:$CLASSPATH
+export CLASSPATH=.:~/Apps/antlr4-4.13.1-complete.jar:$CLASSPATH
 alias grun='java org.antlr.v4.gui.TestRig'
+
+export ANTLR4HOME=/home/lmcs/Apps/antlr4-cpp/run
+export ANTLR4I=/home/lmcs/Apps/antlr4-cpp/run/usr/local/include/antlr4-runtime
+export ANTLR4L=/home/lmcs/Apps/antlr4-cpp/run/usr/local/lib/
+
