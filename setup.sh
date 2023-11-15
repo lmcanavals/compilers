fafa5fd4c482a7215e78901b85f950816071a698
Luis M Canaval S <public02@canaval.org>
2023-11-15 07:24:18 -0500

    clean envs

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ alias antlr4="java org.antlr.v4.Tool"
 alias grun='java org.antlr.v4.gui.TestRig'
 
 export ANTLR4HOME=$MYDEPENDENCIES/antlr4-cpp/run
-export ANTLR4I=$MYDEPENDENCIES/antlr4-cpp/run/usr/local/include/antlr4-runtime
-export ANTLR4L=$MYDEPENDENCIES/antlr4-cpp/run/usr/local/lib/
+export ANTLR4I=$ANTLR4HOME/usr/local/include/antlr4-runtime
+export ANTLR4L=$ANTLR4HOME/usr/local/lib/
 
