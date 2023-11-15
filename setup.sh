94c870341786b71e39af4baa655ed64034e56f3c
Luis M Canaval S <public02@canaval.org>
2023-11-14 23:22:37 -0500

    cmake

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/zsh
 
-export CLASSPATH=.:~/Apps/antlr4-4.13.1-complete.jar:$CLASSPATH
+export MYDEPENDENCIES=$HOME/Apps
+
+export CLASSPATH=.:$MYDEPENDENCIES/antlr-4.13.1-complete.jar:$CLASSPATH
+alias antlr4="java org.antlr.v4.Tool"
 alias grun='java org.antlr.v4.gui.TestRig'
 
-export ANTLR4HOME=/home/lmcs/Apps/antlr4-cpp/run
-export ANTLR4I=/home/lmcs/Apps/antlr4-cpp/run/usr/local/include/antlr4-runtime
-export ANTLR4L=/home/lmcs/Apps/antlr4-cpp/run/usr/local/lib/
+export ANTLR4HOME=$MYDEPENDENCIES/antlr4-cpp/run
+export ANTLR4I=$MYDEPENDENCIES/antlr4-cpp/run/usr/local/include/antlr4-runtime
+export ANTLR4L=$MYDEPENDENCIES/antlr4-cpp/run/usr/local/lib/
 
