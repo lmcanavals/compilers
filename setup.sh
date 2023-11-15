ce6e552c50e796b869f960ea13a8c20f2205c1fa
Luis M Canaval S <public02@canaval.org>
2023-11-15 07:48:29 -0500

    build w cmake against llvm17 from source

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/zsh
 
 export MYDEPENDENCIES=$HOME/Apps
+export LLVMDEPENDENCIES=$HOME/Documents/git/llvm-project/build
 
 export CLASSPATH=.:$MYDEPENDENCIES/antlr-4.13.1-complete.jar:$CLASSPATH
 alias antlr4="java org.antlr.v4.Tool"
@@ -9,4 +10,7 @@ alias grun='java org.antlr.v4.gui.TestRig'
 export ANTLR4HOME=$MYDEPENDENCIES/antlr4-cpp/run
 export ANTLR4I=$ANTLR4HOME/usr/local/include/antlr4-runtime
 export ANTLR4L=$ANTLR4HOME/usr/local/lib/
+
+export LLVM17I=$LLVMDEPENDENCIES/include
+export LLVM17L=$LLVMDEPENDENCIES/lib
 
